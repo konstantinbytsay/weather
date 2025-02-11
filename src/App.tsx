a817ba21adc2a17f0acd47308318eb89561413bc
BytsaiKonstantin <91729673+kbytsai@users.noreply.github.com>
2025-02-11 22:00:59 +0300

    feat(sass): Added sass to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routesConfig } from "./routes/routes.config";
+import "@src/styles/reset.scss";
 
 const App: React.FC = () => {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import vitePluginSass from "vite-plugin-sass";
 // Получаем __dirname в среде ES-модулей
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginSass()],
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
